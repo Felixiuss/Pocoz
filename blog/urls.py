a568a620a1f688c99b6a2e20aa152a985a4318fb
felix <ramses767@gmail.com>
2020-06-02 23:12:23 +0300

    Add tags using library django-taggit

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,9 @@ from . import views
 
 urlpatterns = [
     # post views
-    # path('', views.post_list, name='post_list'),
-    path('', views.PostListView.as_view(), name='post_list'),  # все посты
+    path('', views.post_list, name='post_list'),
+    url(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name='post_list_by_tag'),
+    # path('', views.PostListView.as_view(), name='post_list'),  # все посты
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'r'(?P<post>[-\w]+)/$',
         views.post_detail,
         name='post_detail'),
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView
 
 from .forms import EmailPostForm, CommentForm
 from .models import Post, Comment
+from taggit.models import Tag
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
@@ -15,20 +16,29 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'  # по умолчанию blog/post_list.html
 
 
-# def post_list(request):
-#     """Представление на основе функции"""
-#     object_list = Post.objects.all()
-#     paginator = Paginator(object_list, 3)  # 3 posts in each page
-#     page = request.GET.get('page')  # запрос номера страници
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         # If page is not an integer deliver the first page
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # If page is out of range deliver last page of results
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+def post_list(request, tag_slug=None):
+    """Представление на основе функции"""
+    object_list = Post.objects.all()
+    tag = None
+
+    # если в строке запроса есть tag/... - выводим список только тех постов в которых есть данный тег
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
+    page = request.GET.get('page')  # запрос номера страници
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page,
+                                                   'posts': posts,
+                                                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
