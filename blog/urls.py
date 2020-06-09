91bffaafad4b733333f9b1c9d528f3164fbc0d98
felix <ramses767@gmail.com>
2020-06-09 16:14:16 +0300

    Создание новостного канала для блога

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.urls import path
 
 from . import views
+from .feeds import LatestPostsFeed
 
 urlpatterns = [
     # post views
@@ -11,6 +12,7 @@ urlpatterns = [
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'r'(?P<post>[-\w]+)/$',
         views.post_detail,
         name='post_detail'),
-    url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
+    url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),  # поделиться по e-mail
+    url(r'^feed/$', LatestPostsFeed(), name='post_feed'),  # RSS канал последних новостей
 ]
 # TODO change to path
